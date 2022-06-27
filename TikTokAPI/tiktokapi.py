0fbd76b7f114222dae172c0a966922440ccc4c10
Rick Getz <richardgetziii@gmail.com>
2022-06-26 23:12:36 -0400

    Updated to work

--- TikTokAPI/tiktokapi.py
+++ TikTokAPI/tiktokapi.py
@@ -2,7 +2,13 @@ import os
 import string
 import random
 import urllib.parse
-from .utils import random_key, build_get_url, get_req_json, get_req_content, get_req_text
+from .utils import (
+    random_key,
+    build_get_url,
+    get_req_json,
+    get_req_content,
+    get_req_text,
+)
 from .tiktok_browser import TikTokBrowser
 
 
@@ -11,21 +17,28 @@ class VideoException(Exception):
 
 
 class TikTokAPI(object):
-
-    def __init__(self, cookie=None, language='en', browser_lang="en-US", timezone="Asia/Kolkata", region='IN'):
+    def __init__(
+        self,
+        cookie=None,
+        language="en",
+        browser_lang="en-US",
+        timezone="America/New_York",
+        region="US",
+    ):
         self.base_url = "https://t.tiktok.com/api"
         self.user_agent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:79.0) Gecko/20100101 Firefox/79.0"
 
         if cookie is None:
             cookie = {}
-        self.verifyFp = cookie.get("s_v_web_id", "verify_kjf974fd_y7bupmR0_3uRm_43kF_Awde_8K95qt0GcpBk")
+        self.verifyFp = cookie.get(
+            "s_v_web_id", "verify_kjf974fd_y7bupmR0_3uRm_43kF_Awde_8K95qt0GcpBk"
+        )
         self.tt_webid = cookie.get("tt_webid", "6913027209393473025")
-
         self.headers = {
-            'Host': 't.tiktok.com',
-            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:79.0) Gecko/20100101 Firefox/79.0',
-            'Referer': 'https://www.tiktok.com/',
-            'Cookie': 'tt_webid_v2={}; tt_webid={}'.format(self.tt_webid, self.tt_webid)
+            "Host": "us.tiktok.com",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
+            "Referer": "https://www.tiktok.com/",
+            "Cookie": "tt_webid_v2={}; ttwid={};".format(self.tt_webid, self.tt_webid),
         }
         self.language = language
         self.browser_lang = browser_lang
@@ -34,34 +47,32 @@ class TikTokAPI(object):
 
         self.default_params = {
             "aid": "1988",
+            "authority": "us.tiktok.com",
             "app_name": "tiktok_web",
-            "device_platform": "web",
+            "app_language": "en",
+            "device_platform": "web_pc",
             "referer": "",
             "user_agent": urllib.parse.quote_plus(self.user_agent),
             "cookie_enabled": "true",
             "screen_width": "1920",
             "screen_height": "1080",
             "browser_language": self.browser_lang,
-            "browser_platform": "Linux+x86_64",
+            "browser_platform": "MacIntel",
             "browser_name": "Mozilla",
-            "browser_version": "5.0+(X11)",
+            "browser_version": "5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
             "browser_online": "true",
-            "timezone_name": self.timezone,
+            "tz_name": self.timezone,
+            "webcast_language": "en",
+            "root_referer": "https://www.tiktok.com/logout?redirect_url=https%3A%2F%2Fwww.tiktok.com%2Fforyou",
             # "page_referer": "https://www.tiktok.com/foryou?lang=en",
             "priority_region": self.region,
-
-            "appId": "1180",
+            # "appId": "1180",
             "region": self.region,
             "appType": "t",
-
-            "isAndroid": "false",
-            "isMobile": "false",
-            "isIOS": "false",
-            "OS": "linux",
+            "os": "mac",
             "tt-web-region": self.region,
-
             "language": self.language,
-            "verifyFp": self.verifyFp
+            "verifyFp": self.verifyFp,
         }
         self.signature_key = "_signature"
         self.did_key = "did"
@@ -69,7 +80,7 @@ class TikTokAPI(object):
 
     def send_get_request(self, url, params, extra_headers=None):
         url = build_get_url(url, params)
-        did = ''.join(random.choice(string.digits) for num in range(19))
+        did = "".join(random.choice(string.digits) for num in range(19))
         url = build_get_url(url, {self.did_key: did}, append=True)
         signature = self.tiktok_browser.fetch_auth_params(url, language=self.language)
         url = build_get_url(url, {self.signature_key: signature}, append=True)
@@ -84,6 +95,25 @@ class TikTokAPI(object):
         data = get_req_json(url, params=None, headers=self.headers)
         return data
 
+    def search_get_request(self, url, params, extra_headers=None):
+        url = build_get_url(url, params)
+        did = "".join(random.choice(string.digits) for num in range(19))
+        url = build_get_url(url, {"search_id": did}, append=True)
+        signature = self.tiktok_browser.fetch_auth_params(url, language=self.language)
+        url = build_get_url(url, {self.signature_key: signature}, append=True)
+        if extra_headers is None:
+            headers = self.headers
+        else:
+            headers = {}
+            for key, val in extra_headers.items():
+                headers[key] = val
+            for key, val in self.headers.items():
+                headers[key] = val
+        print(url)
+        print(self.headers)
+        data = get_req_json(url, params=None, headers=self.headers)
+        return data
+
     def getTrending(self, count=30):
         url = self.base_url + "/item_list/"
         req_default_params = {
@@ -94,9 +124,7 @@ class TikTokAPI(object):
             "minCursor": "0",
             "sourceType": "12",
         }
-        params = {
-            "count": str(count)
-        }
+        params = {"count": str(count)}
         for key, val in req_default_params.items():
             params[key] = val
         for key, val in self.default_params.items():
@@ -126,11 +154,7 @@ class TikTokAPI(object):
             "minCursor": "0",
             "sourceType": "8",
         }
-        params = {
-            "id": user_id,
-            "secUid": secUid,
-            "count": str(count)
-        }
+        params = {"id": user_id, "secUid": secUid, "count": str(count)}
         for key, val in req_default_params.items():
             params[key] = val
         for key, val in self.default_params.items():
@@ -150,11 +174,7 @@ class TikTokAPI(object):
             "minCursor": "0",
             "sourceType": "9",
         }
-        params = {
-            "id": user_id,
-            "secUid": secUid,
-            "count": str(count)
-        }
+        params = {"id": user_id, "secUid": secUid, "count": str(count)}
         for key, val in req_default_params.items():
             params[key] = val
         for key, val in self.default_params.items():
@@ -163,14 +183,12 @@ class TikTokAPI(object):
 
     def getHashTag(self, hashTag):
         url = self.base_url + "/challenge/detail/"
-        params = {
-            "challengeName": hashTag.replace("#", "")
-        }
+        params = {"challengeName": hashTag.replace("#", "")}
         for key, val in self.default_params.items():
             params[key] = val
         return self.send_get_request(url, params)
 
-    def getVideosByHashTag(self, hashTag, count=30):
+    def getVideosByHashTag(self, hashTag, count=30, cursor=0):
         hashTag = hashTag.replace("#", "")
         hashTag_obj = self.getHashTag(hashTag)
         hashTag_id = hashTag_obj["challengeInfo"]["challenge"]["id"]
@@ -181,12 +199,12 @@ class TikTokAPI(object):
             "minCursor": "0",
             "maxCursor": "0",
             "shareUid": "",
-            "recType": ""
+            "recType": "",
         }
         params = {
             "challengeID": str(hashTag_id),
             "count": str(count),
-            "cursor": "0",
+            "cursor": str(cursor),
         }
         for key, val in req_default_params.items():
             params[key] = val
@@ -195,11 +213,25 @@ class TikTokAPI(object):
         extra_headers = {"Referer": "https://www.tiktok.com/tag/" + str(hashTag)}
         return self.send_get_request(url, params, extra_headers=extra_headers)
 
+    def getVideosBySearch(self, search, cursor=0):
+        url = "https://us.tiktok.com/api" + "/search/general/full/"
+        # req_default_params = {}
+        params = {
+            "keyword": str(search),
+            "offset": str(cursor),
+            "from_page": "search",
+            "X-Bogus": "DFSzswjYeovANSF0SfHv7Y/F6qyx",
+        }
+        # for key, val in req_default_params.items():
+        #     params[key] = val
+        for key, val in self.default_params.items():
+            params[key] = val
+        extra_headers = {"Referer": "https://www.tiktok.com/"}
+        return self.search_get_request(url, params, extra_headers=extra_headers)
+
     def getMusic(self, music_id):
         url = self.base_url + "/music/detail/"
-        params = {
-            "musicId": music_id
-        }
+        params = {"musicId": music_id}
         for key, val in self.default_params.items():
             params[key] = val
         return self.send_get_request(url, params)
@@ -212,7 +244,7 @@ class TikTokAPI(object):
             "minCursor": "0",
             "maxCursor": "0",
             "shareUid": "",
-            "recType": ""
+            "recType": "",
         }
         params = {
             "musicID": str(music_id),
@@ -223,14 +255,14 @@ class TikTokAPI(object):
             params[key] = val
         for key, val in self.default_params.items():
             params[key] = val
-        extra_headers = {"Referer": "https://www.tiktok.com/music/original-sound-" + str(music_id)}
+        extra_headers = {
+            "Referer": "https://www.tiktok.com/music/original-sound-" + str(music_id)
+        }
         return self.send_get_request(url, params, extra_headers=extra_headers)
 
     def getVideoById(self, video_id):
         url = self.base_url + "/item/detail/"
-        params = {
-            "itemId": str(video_id)
-        }
+        params = {"itemId": str(video_id)}
         for key, val in self.default_params.items():
             params[key] = val
         return self.send_get_request(url, params)
@@ -239,7 +271,7 @@ class TikTokAPI(object):
         video_info = self.getVideoById(video_id)
         video_url = video_info["itemInfo"]["itemStruct"]["video"]["playAddr"]
         video_data = get_req_content(video_url, params=None, headers=self.headers)
-        with open(save_path, 'wb') as f:
+        with open(save_path, "wb") as f:
             f.write(video_data)
 
     def downloadVideoByIdNoWatermark(self, video_id, save_path):
@@ -249,10 +281,15 @@ class TikTokAPI(object):
         pos = video_data.find("vid:")
         if pos == -1:
             raise VideoException("Video without watermark not available in new videos")
-        video_url_no_wm = "https://api2-16-h2.musical.ly/aweme/v1/play/?video_id={" \
-                          "}&vr_type=0&is_play_url=1&source=PackSourceEnum_PUBLISH&media_type=4" \
-            .format(video_data[pos + 4:pos + 36])
+        video_url_no_wm = (
+            "https://api2-16-h2.musical.ly/aweme/v1/play/?video_id={"
+            "}&vr_type=0&is_play_url=1&source=PackSourceEnum_PUBLISH&media_type=4".format(
+                video_data[pos + 4 : pos + 36]
+            )
+        )
 
-        video_data_no_wm = get_req_content(video_url_no_wm, params=None, headers=self.headers)
-        with open(save_path, 'wb') as f:
+        video_data_no_wm = get_req_content(
+            video_url_no_wm, params=None, headers=self.headers
+        )
+        with open(save_path, "wb") as f:
             f.write(video_data_no_wm)
